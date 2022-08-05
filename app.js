a883498f878764848a2b04786208ce3ee423bb43
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-08-04 19:52:57 -0500

    added function to render goblins

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
-// import functions and grab DOM elements
 
-// let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const goblinListEl = document.querySelector('.goblin-list');
+
+let goblinHP = 5;
+
+const goblinsArr = [
+    {
+        name: 'Jeff',
+        HP: 5,
+    },
+    {
+        name: 'Sara',
+        HP: 6,
+    },
+    {
+        name: 'Brodie',
+        HP: 5,
+    },
+];
+
+for (let goblin of goblinsArr) {
+
+    const goblinEl = renderGoblin(goblin);
+    
+    goblinEl.addEventListener('click', () => {
+        if (Math.random() > .5) 
+            alert(`You hit ${goblin.name}!`); // need to subtract HP from goblins when hit
+    });
+
+    goblinListEl.append(goblinEl);
+}
+
+
+function renderGoblin(newGoblin) {
+
+    const nameDiv = document.createElement('div');
+    const hpTag = document.createElement('p');
+
+    nameDiv.textContent = newGoblin.name;
+    hpTag.textContent = `HP: (${newGoblin.HP})`;
+    nameDiv.classList.add('goblin');
+
+    nameDiv.append(hpTag);
+    return nameDiv;
+}
+
+// set default HP for when the new goblin generates upon add challenger 
