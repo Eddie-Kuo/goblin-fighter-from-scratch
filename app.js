33eb35fdd7b1fab9e56693de3bf6958be032ba00
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-08-08 12:52:14 -0500

    fixed the scoring issue when player and goblins have died

--- app.js
+++ app.js
@@ -9,11 +9,8 @@ const userInputEl = document.querySelector('.user-input');
 const userAddButt = document.querySelector('.user-button');
 const userNameEl = document.querySelector('.player-name');
 
-
-
 const playerHealthEl = document.querySelector('.player-health');
 const playerAvatarEl = document.querySelector('.player-avatar');
-
 
 let goblinsSlain = 0;
 let playerHP = 3;
@@ -34,16 +31,15 @@ const goblinsArr = [
 ];
 
 // function to be able input your name as the player
-userAddButt.addEventListener('click', () =>{
+userAddButt.addEventListener('click', () => {
     userNameEl.textContent = `${userInputEl.value} the Minion Purifier`;
 });
-
 
 // Input and button to add in new challengers to the goblin list
 function addGoblin() {
     const newChallenger = {
         name: goblinInputEl.value,
-        HP: 3
+        HP: 3,
     };
     goblinsArr.push(newChallenger);
     renderGoblin(newChallenger);
@@ -54,42 +50,38 @@ goblinAddButt.addEventListener('click', () => {
     displayGoblins();
 });
 
-//function to display goblins and control of their event listeners to manipulate HP 
+//function to display goblins and control of their event listeners to manipulate HP
 function displayGoblins() {
     goblinListEl.textContent = '';
 
     for (let goblin of goblinsArr) {
-
         const goblinEl = renderGoblin(goblin);
-  
-        goblinEl.addEventListener('click', () => {
-            if (Math.random() > .5) {
-                alert(`You hit ${goblin.name}!`);
-                goblin.HP--;}
-            if (Math.random() < .8) {
-                alert(`You have been hit by ${goblin.name}!`);
-                playerHP--;
-                playerHealthEl.textContent = `HP: (${playerHP})`;
-            }
-            if (playerHP === 0) {
-                alert(`You have been defeated!!`);
-                playerAvatarEl.textContent = `😵`;
-            }
-            if (goblin.HP === 0) {
-                goblinsSlain++;
-                goblinEl.disabled = true;
-                goblinScoreEl.textContent = `You have purified ${goblinsSlain} minions(s)`;
-                alert(`You have defeated ${goblin.name}`);
-            }
 
-            displayGoblins();
-        });
+        if (goblin.HP > 0) {
+            goblinEl.addEventListener('click', () => {
+                if (Math.random() > 0.3 && goblin.HP > 0 && playerHP > 0) {
+                    alert(`You hit ${goblin.name}!`);
+                    goblin.HP--;
+                }
+                if (Math.random() < 0.9 && playerHP > 0 && goblin.HP > 0) {
+                    alert(`You have been hit by ${goblin.name}!`);
+                    playerHP--;
+                    playerHealthEl.textContent = `HP: (${playerHP})`;
+                }
+                if (playerHP === 0) {
+                    alert(`You have been defeated!!`);
+                    playerAvatarEl.textContent = `😵`;
+                }
+                if (goblin.HP === 0) {
+                    goblinsSlain++;
+                    goblinScoreEl.textContent = `You have purified ${goblinsSlain} minion(s)`;
+                    alert(`You have defeated ${goblin.name}`);
+                }
+                displayGoblins();
+            });
+        }
         goblinListEl.append(goblinEl);
     }
-  
 }
-
-
-
 
 displayGoblins();
